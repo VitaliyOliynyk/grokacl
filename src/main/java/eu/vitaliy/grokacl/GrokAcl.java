4839048f3d75b82b8aaa3aae2868e3c2d4cb8cfe
VitaliyOliynyk <xaoc00@gmail.com>
2017-09-21 23:02:56 +0200

    add files

---
+++ src/main/java/eu/vitaliy/grokacl/GrokAcl.java
@@ -0,0 +1,32 @@
+package eu.vitaliy.grokacl;
+import org.opensolaris.opengrok.authorization.IAuthorizationPlugin;
+import org.opensolaris.opengrok.configuration.Group;
+import org.opensolaris.opengrok.configuration.Project;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Created by xaoc on 19.09.17.
+ */
+public class GrokAcl implements IAuthorizationPlugin {
+    @Override
+    public void load() {
+        System.out.println("Hello! load");
+    }
+
+    @Override
+    public void unload() {
+        System.out.println("Hello! unload");
+    }
+
+    @Override
+    public boolean isAllowed(HttpServletRequest httpServletRequest, Project project) {
+        return true;
+    }
+
+    @Override
+    public boolean isAllowed(HttpServletRequest httpServletRequest, Group group) {
+        return true;
+    }
+}
